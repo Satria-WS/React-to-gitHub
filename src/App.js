6be36612e75af3f6127fb6e4e4e96cbabc4b909a
Satria-WS <extencezone02@gmail.com>
2022-05-26 17:31:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ function App() {
       <img src={cloud} />
       <img src={cloud} />
       <img src={cloud} />
-      <h1>Images</h1>
       <img src={cloud} />
       <img src={cloud} />
-      <img src={cloud} /> <img src={cloud} />
+      <img src={cloud} />
+      <img src={cloud} />
       <img src={cloud} />
       <img src={cloud} />
     </div>
